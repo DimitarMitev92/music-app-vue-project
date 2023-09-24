aa0cc507bc8e46e3fa7c3777aed7654eac61902f
Dimitar Mitev <d.mitev920123@gmail.com>
2023-09-24 13:26:06 +0300

    Setup Tailwindcss and esLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,12 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier'
   ],
   overrides: [
     {
@@ -18,7 +18,10 @@ module.exports = {
       ]
     }
   ],
+  env: {
+    node: true
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
-}
+};
